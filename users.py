fcbcf6675452e6e1c08ea121971d0ae5f1c6a132
Avenger2256 <146481813+Avenger2256@users.noreply.github.com>
2025-04-22 11:20:01 +0300

    STRINGS

--- users.py
+++ users.py
@@ -1,64 +1,77 @@
-import random
 from config import *
 from random import choice
 from loguru import logger
 
 logger.add('logs/log_{time:DD.MM.YYYY}.log', retention='7 days')
 
+l = load('str')['log']
+
 def findRandom(userId: str):
     data = load()
     randomId = choice(list(data.keys()))
     data = data[randomId]
     if data['ban'] == False and randomId != userId:
-        logger.info(f'{userId} нашел {data['id']}')
+        if l['find']:
+            logger.info(l['find'].format(id = userId, id2 = data['']))
         return data
     else:
         return findRandom(userId)
 
-def getById(userId: str):
+def getById(userId: str, fromUserId: str):
     data = load()
     if userId not in data:
+        if l['errFindId']:
+            logger.error(l['errFindId'].format(id = fromUserId, id2 = userId))
         return
     else:
+        if l['findId']:
+            logger.success(l['findId'].format(id = fromUserId, id2 = userId))
         return data[userId]
 
 def addUser(userId: str, description: str):
     data = load()
     if userId not in data:
-        logger.info(f'{userId} создал анкету | {description}')
+        if l['create']:
+            logger.info(l['create'].format(id = userId, description = description))
         data[userId] = {
             'id': userId,
             'description': description,
             'ban': False
         }
     else:
-        logger.info(f'{userId} обновил анкету | {description}')
+        if l['update']:
+            logger.info(l['update'].format(id = userId, description = description))
         data[userId]['description'] = description
     save(data)
 
 def deleteUser(userId: str):
     data = load()
     if userId not in data:
-        logger.error(f'Не удалось удалить анкету {userId} | 404')
+        if l['deleteError']:
+            logger.error(l['deleteError'].format(id=userId))
         return
     else:
         data.pop(userId)
         save(data)
-        logger.succes(f'Анкета {userId} удалена')
+        if l['delete']:
+            logger.error(l['delete'].format(id=userId))
 def banUser(userId: str):
     data = load()
     if userId not in data:
-        logger.error(f'Не удалось забанить {userId} | 404')
-        return
+        addUser(userId, 'Auto created for ban')
+        banUser(userId)
     else:
         data[userId]['ban'] = True
         save(data)
-        logger.success(f'Пользователь {userId} забанен')
+        if l['ban']:
+            logger.success(l['ban'].format(id=userId))
 
 def unbanUser(userId: str):
     data = load()
-    if userId not in data:
-        logger.error(f'Не удалось разбанить {userId} | 404')
+    admins = load('cfg')['admins']
+    if userId not in data or admins[userId] == 2:
+        if l['unbanError']:
+            logger.error(f'Не удалось разбанить {userId} | 404')
         return
     else:
         data[userId]['ban'] = False
